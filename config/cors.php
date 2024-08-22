83ac27dd621b25d14e9c29c990f57b24e156297d
@tsabit <tsabitimanana55@gmail.com>
2024-08-23 00:40:19 +0700

    fix all

--- config/cors.php
+++ config/cors.php
@@ -2,33 +2,20 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // Mengizinkan semua metode HTTP (GET, POST, PUT, DELETE, dll.)
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*'], // Menambahkan localhost dan 127.0.0.1
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // Mengizinkan semua header
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => false, // Jika tidak menggunakan credentials seperti cookies atau auth
 
 ];
